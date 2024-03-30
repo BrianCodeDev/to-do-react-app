c08004cb0a94286e98ef154a4525d3831278a332
Brian Kelley <107327227+BrianCodeDev@users.noreply.github.com>
2024-03-30 15:16:24 -0500

    update 3.0
    Finished so far the InputFeild.tsx file

--- typescript-tutorial/src/App.tsx
+++ typescript-tutorial/src/App.tsx
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import InputFeild from './components/InputFeild';
+import { Todo } from "./model";
 
-function App() {
-  return <div className='App'>
-    <span className='heading'>Task It</span>
-    <InputFeild />
-    </div>
-}
+ const App: React.FC = () => {
+ const [todo, setTodo] = useState<string>("");
+ const [todos, setTodos] = useState<Todo[]>([]);
+
+ const handleAdd = (e: React.FormEvent) => {
+  e.preventDefault();
+
+  if (todo) {
+    setTodos([...todos, { id: Date.now(), todo, isDone: false }])
+    setTodo("");
+  }
+
+
+ };
+
+ console.log(todos);
+
+  return (
+    <div className='App'>
+      <span className='heading'>Task It</span>
+      <InputFeild todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
+      {/* <TodoList /> */}
+      </div>
+      );
+ }
+
 
 export default App;
--- typescript-tutorial/src/components/InputFeild.tsx
+++ typescript-tutorial/src/components/InputFeild.tsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import './style.css'
 
-const InputFeild = () => {
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
+}
+
+const InputFeild: React.FC<Props> = ({ todo, setTodo, handleAdd }: Props) => {
+const InputRef = useRef<HTMLInputElement>(null);
+
   return (
-    <div>
+    <form className='input' 
+      onSubmit={(e) => {
+      handleAdd(e);
+      InputRef.current?.blur();
+    }}>
+        <input 
+        ref={InputRef}
+        type="input"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+        placeholder='Enter a task' className='input__box'/>
+        <button className='input__submit' type='submit'>Go</button>
       
-    </div>
+    </form>
   )
 }
 
