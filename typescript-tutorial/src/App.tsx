42d9eb31b5a5fdbcf926ccde1def69a01fe0243d
Brian Kelley <107327227+BrianCodeDev@users.noreply.github.com>
2024-03-30 21:32:55 -0500

    update 6.0
    Finished the code. Now I have a full drag and drop to do list in react using typescript!

--- typescript-tutorial/src/App.tsx
+++ typescript-tutorial/src/App.tsx
@@ -1,34 +1,69 @@
-import React, { useState } from 'react';
-import './App.css';
-import InputFeild from './components/InputFeild';
+import React, { useState } from "react";
+import "./App.css";
+import InputField from "./components/InputFeild";
+import TodoList from "./components/TodoList";
+import { DragDropContext, DropResult } from "react-beautiful-dnd";
 import { Todo } from "./model";
-import TodoList from './components/TodoList';
 
- const App: React.FC = () => {
- const [todo, setTodo] = useState<string>("");
- const [todos, setTodos] = useState<Todo[]>([]);
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Array<Todo>>([]);
+  const [completedTodos, setCompletedTodos] = useState<Array<Todo>>([]);
 
- const handleAdd = (e: React.FormEvent) => {
-  e.preventDefault();
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
 
-  if (todo) {
-    setTodos([...todos, { id: Date.now(), todo, isDone: false }])
-    setTodo("");
-  }
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
+      setTodo("");
+    }
+  };
 
+  const onDragEnd = (result: DropResult) => {
+    const { destination, source } = result;
 
- };
+    console.log(result);
 
- console.log(todos);
+    if (!destination) {
+      return;
+    }
+
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    const updatedTodos = Array.from(todos);
+    const updatedCompletedTodos = Array.from(completedTodos);
+
+    const removedTodo = source.droppableId === "TodosList" ? updatedTodos.splice(source.index, 1)[0] : updatedCompletedTodos.splice(source.index, 1)[0];
+
+    if (destination.droppableId === "TodosList") {
+      updatedTodos.splice(destination.index, 0, removedTodo);
+    } else {
+      updatedCompletedTodos.splice(destination.index, 0, removedTodo);
+    }
+
+    setTodos(updatedTodos);
+    setCompletedTodos(updatedCompletedTodos);
+  };
 
   return (
-    <div className='App'>
-      <span className='heading'>Task It</span>
-      <InputFeild todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
-      <TodoList todos={todos} setTodos={setTodos} />
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div className="App">
+        <span className="heading">Taskify</span>
+        <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+        <TodoList
+          todos={todos}
+          setTodos={setTodos}
+          completedTodos={completedTodos}
+          setCompletedTodos={setCompletedTodos}
+        />
       </div>
-      );
- }
-
+    </DragDropContext>
+  );
+};
 
 export default App;
--- typescript-tutorial/src/components/InputFeild.tsx
+++ typescript-tutorial/src/components/InputFeild.tsx
@@ -1,31 +1,36 @@
-import React, { useRef } from 'react';
-import './style.css';
+import React, { useRef } from "react";
+import "./style.css";
 
-interface Props {
+interface props {
   todo: string;
   setTodo: React.Dispatch<React.SetStateAction<string>>;
   handleAdd: (e: React.FormEvent) => void;
 }
 
-const InputFeild: React.FC<Props> = ({ todo, setTodo, handleAdd }: Props) => {
-const InputRef = useRef<HTMLInputElement>(null);
+const InputField: React.FC<props> = ({ todo, setTodo, handleAdd }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
 
   return (
-    <form className='input' 
+    <form
+      className="input"
       onSubmit={(e) => {
-      handleAdd(e);
-      InputRef.current?.blur();
-    }}>
-        <input 
-        ref={InputRef}
-        type="input"
+        handleAdd(e);
+        inputRef.current?.blur();
+      }}
+    >
+      <input
+        type="text"
+        placeholder="Enter a Task"
         value={todo}
+        ref={inputRef}
         onChange={(e) => setTodo(e.target.value)}
-        placeholder='Enter a task' className='input__box'/>
-        <button className='input__submit' type='submit'>Go</button>
-      
+        className="input__box"
+      />
+      <button type="submit" className="input_submit">
+        GO
+      </button>
     </form>
-  )
-}
+  );
+};
 
-export default InputFeild
+export default InputField;
